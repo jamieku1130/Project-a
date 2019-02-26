5ae314bb488cafea97f122d4337bcfc6c2470766
Vic Huang <vic.huang1130x10@gmail.com>
2019-02-26 15:28:41 +0800

    add media-query @768 for landing

--- src/ProjectA/Components/AnswerCard/connect.js
+++ src/ProjectA/Components/AnswerCard/connect.js
@@ -1,7 +1,6 @@
 import { connect } from "react-redux";
 import { answerQuiz } from "../../data/appState";
 
-// const mapStateToProps = state => ({ answerStatus: state.app.answerStatus });
 const mapDispatchToProps = { answerQuiz };
 
 export default connect(
